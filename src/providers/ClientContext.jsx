898b18b13012b1c12ed3b0f271447c221420aa71
Karengrabarz <karengrabarz@gmail.com>
2024-02-08 15:39:21 -0300

    fix: delete console.log

--- src/providers/ClientContext.jsx
+++ src/providers/ClientContext.jsx
@@ -122,7 +122,6 @@ export const ClientProvider = ({ children }) => {
         navigate("/");
       }, 2000);
     } catch (error) {
-      console.log(error.response.data.error)
       toast.error(error.response?.data.error);
     } finally {
       setLoading(false);
